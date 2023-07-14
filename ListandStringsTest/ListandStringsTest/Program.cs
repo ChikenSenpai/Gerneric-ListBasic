48b12236de0a06bf5823a7893f2c1350d3d8b6bd
ChikenSenpai <130470269+ChikenSenpai@users.noreply.github.com>
2023-07-14 11:21:18 +0200

    Add files via upload

---
+++ ListandStringsTest/ListandStringsTest/Program.cs
@@ -0,0 +1,29 @@
+﻿
+string itemInput;
+
+List<string> shopList = new List<string>();
+
+Console.WriteLine("Enter x to cancel");
+do
+{
+    itemInput = Console.ReadLine().ToLower();
+    if(itemInput == "x")
+    {
+
+    }
+    else
+    {
+        //capitalizes the first letter of a string (Substring adds the rest of the string to the capital letter)
+        itemInput = char.ToUpper(itemInput[0]) + itemInput.Substring(1); 
+        Console.WriteLine($"{itemInput} was added to List");
+        shopList.Add(itemInput);
+    }  
+
+}while (itemInput != "x");
+
+
+foreach(var item in shopList) 
+{ 
+    Console.WriteLine(item);
+}
+
